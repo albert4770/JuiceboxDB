d77d371f16255f5bad771f11393453ae8c680ab5
Beto <albert.4770@gmail.com>
2020-09-17 17:01:30 -0600

    create and update funcs

---
+++ db/index.js
@@ -0,0 +1,134 @@
+const { Client } = require('pg');
+
+const db = new Client('postgres://localhost:5432/juicebox-dev');
+
+const getAllUsers = async () => {
+	const { rows } = await db.query(
+		`SELECT id, username, name, location FROM users;`
+	);
+
+	return rows;
+};
+
+const createUser = async ({ username, password, name, location }) => {
+	try {
+		const {
+			rows
+		} = await db.query(
+			`INSERT INTO users(username, password, name, location) VALUES($1, $2, $3, $4) ON CONFLICT (username) DO NOTHING RETURNING *;`,
+			[username, password, name, location]
+		);
+		return rows;
+	} catch (err) {
+		throw err;
+	}
+};
+
+const updateUser = async (id, fields = {}) => {
+	const setString = Object.keys(fields)
+		.map((key, index) => {
+			return `"${key}"=$${index + 1}`;
+		})
+		.join(', ');
+
+	if (setString.length === 0) {
+		return;
+	}
+
+	try {
+		const {
+			rows: [user]
+		} = await db.query(
+			`UPDATE users SET ${setString} WHERE id=${id} RETURNING *;`,
+			Object.values(fields)
+		);
+		return user;
+	} catch (err) {
+		throw err + 'Error Bitch 1';
+	}
+};
+
+const createPost = async ({ authorId, title, context }) => {
+	try {
+		const {
+			rows
+		} = await db.query(
+			`INSERT INTO posts("authorId", title, context) VALUES($1, $2, $3) RETURNING *;`,
+			[authorId, title, context]
+		);
+		return rows;
+	} catch (err) {
+		throw err;
+	}
+};
+
+const updatePost = async (id, { title, context, active }) => {
+	try {
+		const setString = Object.keys({ title, context, active })
+			.map((key, index) => {
+				return `"${key}=$${index + 1}"`;
+			})
+			.join(', ');
+
+		if (setString === 0) {
+			return;
+		}
+
+		const { rows } = await db.query(
+			`UPDATE posts SET ${setString} WHERE id=${id} RETURNING *`
+		);
+		return rows;
+	} catch (err) {
+		throw err;
+	}
+};
+
+const getAllPosts = async () => {
+	try {
+		const { rows } = await db.query(
+			`SELECT id, "authorId", title, context FROM posts`
+		);
+		return rows;
+	} catch (err) {
+		throw err;
+	}
+};
+
+const getPostsByUser = async userId => {
+	try {
+		const { rows } = await db.query(
+			`SELECT * FROM posts WHERE authorId=${userId}`
+		);
+		return rows;
+	} catch (err) {
+		throw err;
+	}
+};
+
+const getUserById = async userId => {
+	try {
+		const { rows } = await db.query(
+			`SELECT * FROM users WHERE id=${userId}`
+		);
+
+		if (!rows) {
+			return null;
+		}
+
+		delete rows.password;
+		rows.posts = getPostsByUser(userId);
+		return rows;
+	} catch (err) {}
+};
+
+module.exports = {
+	db,
+	getAllUsers,
+	createUser,
+	updateUser,
+	createPost,
+	updatePost,
+	getAllPosts,
+	getPostsByUser,
+	getUserById
+};
---
+++ db/seed.js
@@ -0,0 +1,105 @@
+const { db, getAllUsers, createUser, updateUser, createPost, getAllPosts } = require('./index'); //prettier-ignore
+
+const createInitialUsers = async () => {
+	try {
+		console.log('Creating initial users');
+		const users = await createUser({
+			username: 'beto44',
+			password: '1234',
+			name: 'Albert',
+			location: 'Honduras'
+		});
+		console.log('Users: ', users);
+
+		console.log('Finished creating users');
+	} catch (err) {
+		throw err;
+	}
+};
+
+const createInitialPosts = async () => {
+	try {
+		console.log('Creating initial posts');
+		const { rows } = await createPost({
+			authorId: 1,
+			title: 'First post ever',
+			context: 'Hello worldly beings'
+		});
+		return rows;
+	} catch (err) {
+		// throw ('Error @createPosts', err);
+	}
+};
+
+const dropTables = async () => {
+	try {
+		await db.query(`
+			DROP TABLE IF EXISTS posts; 
+			DROP TABLE IF EXISTS users;`);
+	} catch (err) {
+		throw err;
+	}
+};
+
+const createTables = async () => {
+	try {
+		await db.query(`
+			CREATE TABLE users(
+			id SERIAL PRIMARY KEY,
+			username varchar(255) UNIQUE NOT NULL, 
+			password varchar(255) NOT NULL,
+			name varchar(255) NOT NULL,
+			location varchar(255) NOT NULL,
+			active BOOLEAN DEFAULT TRUE);
+
+			CREATE TABLE posts(
+			id SERIAL PRIMARY KEY,
+			"authorId" INTEGER REFERENCES users(id) NOT NULL,
+			title varchar(255) NOT NULL,
+			context TEXT NOT NULL,
+			active BOOLEAN DEFAULT true);`);
+	} catch (err) {
+		throw err;
+	}
+};
+
+const rebuildDB = async () => {
+	try {
+		db.connect();
+
+		await dropTables();
+		await createTables();
+		await createInitialUsers();
+		await createInitialPosts();
+	} catch (err) {
+		throw err;
+	}
+};
+
+const testDB = async () => {
+	try {
+		console.log('Starting to test db!');
+		const users = await getAllUsers();
+		console.log('All users: ', users);
+
+		console.log('Updating user[0]');
+		const updatedUser = await updateUser(users[0].id, {
+			name: 'Alberto Arriaga'
+		});
+		console.log(updatedUser);
+
+		console.log('Getting all posts');
+		const allPosts = await getAllPosts();
+		console.log(allPosts);
+
+		console.log('Finished testing db!');
+	} catch (err) {
+		console.log('Error bitch 2');
+		throw err;
+	}
+};
+
+rebuildDB()
+	.then(testDB)
+	.catch(console.error)
+	.finally(() => db.end());
